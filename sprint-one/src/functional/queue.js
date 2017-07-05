568474773fbfaae0f96e59f9f2c58c27551f3ee9
Hack Reactor Student <student@hackreactor.com>
2017-07-05 15:47:22 -0700

    Finished functional pattern queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,3 +1,7 @@
+// enqueue(string) - Add a string to the back of the queue
+// dequeue() - Remove and return the string at the front of the queue
+// size() - Return the number of items in the queue
+
 var Queue = function() {
   var someInstance = {};
 
@@ -7,12 +11,22 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.dequeue = function() {
+    var firstItem = storage[Object.keys(storage)[0]];
+
+    delete storage[Object.keys(storage)[0]];
+    for (var key in storage) {
+      storage[key - 1] = storage[key];
+      delete storage[key];
+    }
+    return firstItem;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;

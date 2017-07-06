efc0550dca74e20340c844240dd23eb7820d65c7
Hack Reactor Student <student@hackreactor.com>
2017-07-05 19:59:44 -0700

    Finished functional shared queue and stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,37 @@
+// enqueue(string) - Add a string to the back of the queue
+// dequeue() - Remove and return the string at the front of the queue
+// size() - Return the number of items in the queue
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  
+  _.extend(someInstance, queueMethods);
+  return someInstance;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.size()] = value;
+};
 
+queueMethods.dequeue = function() {
+  var firstItem = this.storage[0];
+  
+  delete this.storage[0];
+  
+  for (var key in this.storage) {
+    this.storage[key - 1] = this.storage[key];
+    delete this.storage[key];
+  }
+  return firstItem;
+};
+
+
+queueMethods.size = function() {
+  return Object.keys(this.storage).length;
+};

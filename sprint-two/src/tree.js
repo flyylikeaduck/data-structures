03b58b3e335d17b610d28c2eda617779ee52cca5
Hack Reactor Student <student@hackreactor.com>
2017-07-07 16:41:04 -0700

    Finished tree!

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -20,68 +20,24 @@ treeMethods.contains = function(target) {
  var childArr = this.children;
  var result = false;
  
-  
-  childArr.forEach(function(childObj) {
-    if (childObj.length === 0) {
-      if (childObj.value === target) {
-        result = true;
-      }
-      return false;
-    //base case
-    } else {
-      //if (result === false) {
-      if (childObj.value === target) {
-        result = true;
-      } else {
-        return childObj.contains(target);
-      }
-    }
-  });
-  //});
-  
+ if (this.value === target) {
+  result = true;
+ }
+ 
+ childArr.forEach(function(childObj) {
+  if (childObj.contains(target)) {
+    result = true;
+  }
+ });
+ 
   return result;
   
-    
-  // childArr.forEach(function(childObj) {
-  //   while (childObj.length > 0) {
-  //     if (childObj.value === target) {
-  //       return true;
-  //     } else {
-  //       childObj.contains(target);
-  //     } 
-  //   }
-  //   if (childObj.value === target) {
-  //     return true;
-  //   }
-  //   return false;
-  // });
 };
-
-// /*
+  
 
 // /*
 //  * Complexity: What is the time complexity of the above functions?
+// addChild: constant
+// contains: linear
 //  */
 
-// {[{value: 5, children: [{value: 6}, {value: 7}, {}]}]}
-
-// var tree = new Tree();
-
-// tree.addChild(5);
-
-// tree.children[0].addChild(6);
-// tree.children[0].addChild(7);
-// expect(tree.children[0].children[0].value).to.equal(6);
-// expect(tree.children[0].children[1].value).to.equal(7);
-
-
-// var tree = new Tree();
-
-// {[{value: 5, children:[ {value: 7, children: null}]}, {value: 6, children: [{value: 8, children: null}] } ]}
-
-// tree.addChild(5);
-// tree.addChild(6);
-// tree.children[0].addChild(7);
-// tree.children[1].addChild(8);
-// expect(tree.contains(7)).to.equal(true);
-// expect(tree.contains(8)).to.equal(true);*/

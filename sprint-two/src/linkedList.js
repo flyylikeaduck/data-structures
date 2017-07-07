19754926e0c807fb85e6466724bee474a95cd662
Hack Reactor Student <student@hackreactor.com>
2017-07-06 21:40:38 -0700

    Finished linkedList, WIP tree -still have last test to pass with recursion

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = new Node(value);
+    
+    if (!list.head) {
+      list.head = node;
+    } else {
+      list.tail.next = node;
+    }
+    
+    list.tail = node;
+    
   };
 
   list.removeHead = function() {
+    var origHead = list.head.value;
+    list.head = list.head.next;
+    return origHead;
   };
 
   list.contains = function(target) {
+    // loop through the nodes
+    var currentNode = list.head;
+    
+    while (currentNode.next) {
+      if (currentNode.value === target) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    if (currentNode.value === target) {
+      return true;
+    }
+    return false;
   };
 
   return list;
@@ -26,4 +53,7 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ // addToTail: constant
+ // removeHead: constant
+ // contains: linear
  */
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,7 +3,8 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  
 
   return newTree;
 };
@@ -11,13 +12,76 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // pushing value as new child into children arr
+  this.children.push(new Tree(value)); 
 };
 
 treeMethods.contains = function(target) {
+ var childArr = this.children;
+ var result = false;
+ 
+  
+  childArr.forEach(function(childObj) {
+    if (childObj.length === 0) {
+      if (childObj.value === target) {
+        result = true;
+      }
+      return false;
+    //base case
+    } else {
+      //if (result === false) {
+      if (childObj.value === target) {
+        result = true;
+      } else {
+        return childObj.contains(target);
+      }
+    }
+  });
+  //});
+  
+  return result;
+  
+    
+  // childArr.forEach(function(childObj) {
+  //   while (childObj.length > 0) {
+  //     if (childObj.value === target) {
+  //       return true;
+  //     } else {
+  //       childObj.contains(target);
+  //     } 
+  //   }
+  //   if (childObj.value === target) {
+  //     return true;
+  //   }
+  //   return false;
+  // });
 };
 
+// /*
+
+// /*
+//  * Complexity: What is the time complexity of the above functions?
+//  */
+
+// {[{value: 5, children: [{value: 6}, {value: 7}, {}]}]}
+
+// var tree = new Tree();
+
+// tree.addChild(5);
+
+// tree.children[0].addChild(6);
+// tree.children[0].addChild(7);
+// expect(tree.children[0].children[0].value).to.equal(6);
+// expect(tree.children[0].children[1].value).to.equal(7);
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+// var tree = new Tree();
+
+// {[{value: 5, children:[ {value: 7, children: null}]}, {value: 6, children: [{value: 8, children: null}] } ]}
+
+// tree.addChild(5);
+// tree.addChild(6);
+// tree.children[0].addChild(7);
+// tree.children[1].addChild(8);
+// expect(tree.contains(7)).to.equal(true);
+// expect(tree.contains(8)).to.equal(true);*/

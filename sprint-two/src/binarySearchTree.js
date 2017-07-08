ce72989dbf65c4d43e664a6167aacab3a2b9eefc
Hack Reactor Student <student@hackreactor.com>
2017-07-08 11:42:32 -0700

    WIP binary search tree --contains and insert

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,17 +1,50 @@
 var BinarySearchTree = function(value) {
-  this.tree = {};
   this.value = value;
-  this.left = new BinarySearchTree();
-  this.right = new BinarySearchTree();
+  this.left = null;
+  this.right = null;
+  
 };
 
+var binarySearchTree = new BinarySearchTree();
 
 BinarySearchTree.prototype.insert = function(value) {
+
+  if (value < this.value) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (!this.right) {
+      this.right = new BinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
   
 };
 
 BinarySearchTree.prototype.contains = function(value) {
-  // return boolean
+  // compare this.value to value, if it matches, return true
+   //if value < this.value, recurse on this.left
+    //if value > this.value, recurse on this.right
+  //return false
+  
+  if (this.value === value) {
+    return true;
+  }
+  
+  if (value < this.value) {
+    this.left.contains(value);
+  }
+  
+  if (value > this.value) {
+    this.right.contains(value);
+  }
+  
+  return false;
+   
 };
 
 BinarySearchTree.prototype.depthFirstLog = function(callback) {
@@ -25,4 +58,3 @@ BinarySearchTree.prototype.depthFirstLog = function(callback) {
  
  // USE CASE:
  */
-

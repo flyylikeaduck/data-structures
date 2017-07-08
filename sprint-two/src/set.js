7b3b73adc50a5826ddf7e9de3ca3ca459c1d8d10
Hack Reactor Student <student@hackreactor.com>
2017-07-07 20:14:41 -0700

    Finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,40 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.contains(item) === false) {
+    this._storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  var result = false;
+  
+  this._storage.forEach(function(element) {
+    if (element === item) {
+      result = true;
+    }
+  });
+  
+  return result;
 };
 
 setPrototype.remove = function(item) {
+  for (var i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ .add is constant
+ .contains & .remove are both linear
+ 
  */
